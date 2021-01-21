9b9153cab98944b74db82428f55710a5f3338106
manasi9307 <manasi.thakur@sjsu.edu>
2021-01-21 15:38:59 -0800

    first commit

---
+++ src/components/Trans.jsx
@@ -0,0 +1,14 @@
+import React , { useContext } from 'react'
+
+import { globalContext } from '../context/globalState';
+
+export const Trans = ({transaction}) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const { deleteTransaction } = useContext(globalContext);
+    return (
+        
+            <li className={transaction.amount > 0 ? "minus" : "plus"}>{ transaction.text } <span>{sign} ${ Math.abs(transaction.amount) }</span>
+            <button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn">X</button></li>
+        
+    )
+}
